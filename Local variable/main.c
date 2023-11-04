e5b91a0d5444c6a19843bdce0a32614d572b7311
QinYang Tan <108197858+TanQinYang@users.noreply.github.com>
2023-11-04 16:00:51 +0100

    Add files via upload

---
+++ Local variable/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+ int a, b;
+
+ void func(int a)
+ {
+ a = 2;
+ b = 3;
+ }
+
+ int main(void)
+ {
+ a = 1;
+ func(a);
+ printf("a: %d, b: %d\n", a, b);
+ return 0;
+ }
+
